47f04e84f07015015221748ed090db4579b1158b
Daniel Wickman & Mumo Musau <pair+dwwickman.mmusau99@devbootcamp.com>
2017-05-29 12:28:26 -0500

    sets up encrypted passswords

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  has_many :rounds
+  has_secure_password
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529161712) do
+ActiveRecord::Schema.define(version: 20170529170714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,5 +30,24 @@ ActiveRecord::Schema.define(version: 20170529161712) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "rounds", force: :cascade do |t|
+    t.bigint "deck_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["deck_id"], name: "index_rounds_on_deck_id"
+    t.index ["user_id"], name: "index_rounds_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   add_foreign_key "cards", "decks"
+  add_foreign_key "rounds", "decks"
+  add_foreign_key "rounds", "users"
 end
